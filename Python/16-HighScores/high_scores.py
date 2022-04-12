c15992d5655713f59c0e97b63f6a7835801e3576
DBSantiago <deblasissantiago@gmail.com>
2022-04-12 19:18:06 -0300

    ADD: /Python/16-HighScores

---
+++ Python/16-HighScores/high_scores.py
@@ -0,0 +1,35 @@
+""" These functions are used to build a high-scores component in a game.
+"""
+
+
+def latest(scores):
+    """
+
+    :param scores: list - all the scores in the game.
+    :return: int - the latest score.
+    """
+    return scores[-1]
+
+
+def personal_best(scores):
+    """
+
+    :param scores: list - all the scores in the game.
+    :return: int - the highest score.
+    """
+    scores.sort()
+
+    return scores[-1]
+
+
+def personal_top_three(scores):
+    """
+
+    :param scores: list - all the scores in the game.
+    :return: list - the 3 highest scores.
+    """
+    scores_copy = list(scores)
+
+    scores_copy.sort()
+
+    return scores_copy[-3:]
